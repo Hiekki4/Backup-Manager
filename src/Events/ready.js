a63e1aecb40a8e36b332dcdf4d367adebe5df433
Ghost <70537353+EvasiveTechGroup@users.noreply.github.com>
2020-12-07 00:01:05 +0000

    Hiekki

--- src/Events/ready.js
+++ src/Events/ready.js
@@ -7,6 +7,13 @@ bot.on("ready", async () => {
 
     console.log(`[LOGIN] - Bot iniciado com ${bot.guilds.cache.size} servidores`.brightCyan);
     console.log(`[LOGIN] - Memoria usada ${(process.memoryUsage().rss/1024/1024).toFixed(2)}MB`.brightCyan)
+    setTimeout(() => {console.log(`
+    >===============================================<
+                    Criado por Hiekki
+    GitHub: https://github.com/Hiekki4
+          Discord: https://discord.gg/B49HQWA8vv
+    >===============================================<`);
+            }, 5000);
     var presences = [
         {name: `Versão atual: [${(config.versão)}]`, type: 'PLAYING'},
         {name: `🔧 Sendo desenvolvido`, type: 'WATCHING'},
